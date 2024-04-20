bd58954c7d9aef916d4450fe7819780fe3fe8126
shaiksha176 <iamshaiksha17@gmail.com>
2024-04-20 09:54:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = process.env.PORT || 3001;
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors("*"));
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Welcome to the Express Application" });
+});
+
+app.use("*", (req, res) => {
+  res.status(404).send({
+    message: "No route exists with the specified path",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
